b4011fc7cb05a2ca768b4f181f4d28d45d5c6cfd
Zaid Said <zeidsaid16@gmail.com>
2020-10-29 01:28:19 +0200

    implement reading url images from JSON file & detecting faces in it

--- 1.py
+++ 1.py
@@ -1,21 +1,57 @@
 import cv2 as cv
+import json
+import codecs
+import numpy as np
+import requests
+import shutil
+from PIL import Image
+from tqdm import tqdm
 
-# Read image from your local file system
-original_image = cv.imread('C:/Users/hp/Pictures/Camera Roll/MZMZ.jpg')
+address = 'D:/Faculty of Engineering/ASU-4 Computer/Graduation Project/face_detection.json'
 
-# Convert color image to grayscale for Viola-Jones
-grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+jsonData = []
 
-face_cascade = cv.CascadeClassifier('F:/ASU/5th year/semester 9/graduation project 1/haarcascade_frontalface_alt.xml')
-detected_faces = face_cascade.detectMultiScale(grayscale_image)
-for (column, row, width, height) in detected_faces:
-    cv.rectangle(
-        original_image,
-        (column, row),
-        (column + width, row + height),
-        (0, 255, 0),
-        2
-    )
-cv.imshow('Image', original_image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+with codecs.open(address, 'rU', 'utf-8') as js:
+    for line in js:
+        jsonData.append(json.loads(line))
+
+del jsonData[272]
+
+images = []
+imagesCount=0
+for data in tqdm(jsonData):
+   
+    response = requests.get(data['content'], stream=True)
+    with open('my_image.jpg', 'wb') as file:
+        shutil.copyfileobj(response.raw, file)
+    del response
+    img = np.asarray(Image.open('my_image.jpg'))
+    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    images.append(img)
+    imagesCount +=1
+    
+print(imagesCount)
+
+count = 0
+detectedFacesCount = 0
+for img in tqdm(images):
+
+    grayscale_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    face_cascade = cv.CascadeClassifier('D:/Faculty of Engineering/ASU-4 Computer/Graduation Project/Face Detection/Face-Detection/haarcascade_frontalface_alt.xml')
+    detected_faces = face_cascade.detectMultiScale(grayscale_image)
+    for (column, row, width, height) in detected_faces:
+        cv.rectangle(
+            img,
+            (column, row),
+            (column + width, row + height),
+            (0, 255, 0),
+            2
+        )
+        detectedFacesCount +=1
+
+    cv.imwrite('./face-detection-images/face_image_{}.jpg'.format(count),img)
+    count += 1
+print("detected", detectedFacesCount, "faces")
+
+print("done")
